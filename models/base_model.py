804b1f4506e4a9ede03e541b7382465513b18f34
bashthat <3249@holbertonschool.com>
2022-06-07 12:42:54 -0700

    edits

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
-            storage.new(self)
+            models.storage.new(self)
 
     def save(self):
         """updates to the current time"""
